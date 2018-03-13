7482e1b733d15562705a5f5665807a7d5dc317c0
Alex <alex.zneue@gmail.com>
2018-03-13 08:55:19 +0300

    Allow to create new pothole records

--- app/controllers/potholes_controller.rb
+++ app/controllers/potholes_controller.rb
@@ -1,2 +1,21 @@
 class PotholesController < ApplicationController
+  def create
+    Pothole.create pothole_params
+  end
+
+  def update
+  end
+
+  private
+
+    def pothole_params
+      params
+        .require(:pothole)
+        .permit(
+          :category,
+          :latitude,
+          :longitude,
+          :fixed_at
+        )
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'maps#index'
+
+  resource :potholes, only: [:create, :update]
 end
