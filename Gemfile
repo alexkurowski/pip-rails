c8c376c429d1fc6a3788a89f41de4085193acc9c
Alex <alex.zneue@gmail.com>
2018-03-13 08:56:02 +0300

    Allow placing new potholes on a map

--- Gemfile
+++ Gemfile
@@ -17,7 +17,13 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
+# Font-awesome icons
+gem 'font-awesome-rails'
+# Add CSS prefixes automatically for wider browser support
+gem 'autoprefixer-rails'
 
+# Use jQuery
+gem 'jquery-rails'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', function () {
   var mapId = "gmap";
 
+  /*
+   * Ensure we have a #gmap element and are able to work with it
+   */
   var mapEl = document.getElementById(mapId);
   if (!mapEl) return;
 
@@ -9,6 +12,39 @@ window.addEventListener('load', function () {
     return;
   }
 
+  /*
+   * Helper functions
+   */
+  var newMarker = null;
+
+  function addDraggableMarker (handler, lat, lng) {
+    if (newMarker) return;
+
+    newMarker = handler.addMarkers([{
+      lat: lat,
+      lng: lng
+    }], {
+      draggable: true
+    });
+  }
+
+  function showOverlay () {
+    document
+      .querySelector('.gmap-overlay')
+      .classList
+      .remove('hidden');
+  }
+
+  function hideOverlay () {
+    document
+      .querySelector('.gmap-overlay')
+      .classList
+      .add('hidden');
+  }
+
+  /*
+   * Initialize google maps widget
+   */
   var styles = [{
     featureType: 'poi',
     elementType: 'labels',
@@ -35,6 +71,7 @@ window.addEventListener('load', function () {
     function () {
       window.map = gmap.serviceObject;
 
+      // Move viewport to visitor's geo location
       navigator
         .geolocation
         .getCurrentPosition(function (position) {
@@ -45,6 +82,49 @@ window.addEventListener('load', function () {
 
           map.setCenter(center);
         });
+
+      // Create a draggable marker on click
+      map.addListener('click', function (event) {
+        addDraggableMarker(
+          handler,
+          event.latLng.lat(),
+          event.latLng.lng()
+        );
+        showOverlay();
+      })
     }
   );
+
+  /*
+   * Initialize buttons functionality
+   */
+  document
+    .querySelectorAll('.add-pothole')
+    .forEach(function (node) {
+      node.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var marker = newMarker[0].serviceObject;
+        var data = {
+          pothole: {
+            latitude:  marker.position.lat(),
+            longitude: marker.position.lng(),
+          }
+        };
+
+        Rails.ajax({
+          type: 'POST',
+          url: '/potholes',
+          data: $.param(data),
+          success: function (response) {
+            marker.setDraggable(false);
+            newMarker = null;
+            hideOverlay();
+          },
+          error: function (response) {
+            console.log("Error occured while creating a pothole record");
+          }
+        });
+      });
+    });
 })
